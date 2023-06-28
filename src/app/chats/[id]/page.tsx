c39ae443f81822c3af157b2dea835678c18ec265
kimilgon <moses011010@gmail.com>
2023-06-28 22:57:25 +0900

    feat: ChatDetail 컴포넌트 구현 1대1 채팅방 페이지 구현

--- src/app/chats/[id]/page.tsx
+++ src/app/chats/[id]/page.tsx
@@ -1,5 +1,16 @@
-const ChatDetailPage = ({ id }: { id: number }) => {
-  return <div>id : {id}</div>;
+import Message from '@/components/Message';
+
+const ChatDetailPage = () => {
+  const testMessage: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return (
+    <div className="w-full">
+      {testMessage.map((item) => (
+        <div className="mb-2 mr-4" key={item}>
+          <Message message="hi" />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ChatDetailPage;
---
+++ src/components/Message.tsx
@@ -0,0 +1,23 @@
+import { cls } from '@/utils/cls';
+
+const Message = ({ message }: { message: string }) => {
+  const isMyMessage = true;
+  return (
+    <div
+      className={cls(
+        'flex items-start space-x-2',
+        isMyMessage ? 'flex-row-reverse space-x-reverse' : 'space-x-2',
+      )}
+    >
+      <div
+        className={cls(
+          'w-auto text-sm h-auto text-dark px-3 py-1 fill-lightGray border border-gray rounded-lg',
+        )}
+      >
+        <p className="break-words whitespace-normal ">{message}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
