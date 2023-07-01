dfa0a5002c4d8d36cf02cf9c340c98a8a52b5628
kimilgon <moses011010@gmail.com>
2023-07-01 14:58:01 +0900

    feat: chat 전송, 수신 기능 구현 zerocho sleact local서버 이용 websocket과 api가 사용자를 식별하지 않고 연결 - 추후 서버 연동시 수정

--- src/app/chats/[id]/layout.tsx
+++ src/app/chats/[id]/layout.tsx
@@ -3,30 +3,7 @@
 import { MouseEventHandler, PropsWithChildren } from 'react';
 
 const ChatRoomLayout = ({ children }: PropsWithChildren) => {
-  const onClick = (e: any) => {
-    e.preventDefault();
-  };
-  return (
-    <div>
-      {children}
-      <form className="fixed inset-x-0 bottom-0 h-10 max-w-xl mx-auto">
-        <div className="relative flex items-center w-full h-full mx-auto bg-gray">
-          <input type="text" className="w-full h-full bg-lightGray" />
-          <button
-            onClick={onClick}
-            className="absolute inset-y-0 flex text-xl bg-none left-2 place-items-center text-gray"
-          >
-            #
-          </button>
-          <div className="absolute inset-y-0 right-0 flex">
-            <button className="px-3 text-white text-md bg-mainPurple focus:ring-2 focus:ring-offset-2 focus:ring-mainPurple hover:bg-mainPurple">
-              &rarr;
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+  return <div>{children}</div>;
 };
 
 export default ChatRoomLayout;
