7208f8334dcadde74b91d30c551354b9465ca6cb
kimilgon <moses011010@gmail.com>
2023-07-03 17:59:54 +0900

    style: default font 변경 Inter -> Noto Sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <div className="w-full h-screen max-w-xl mx-auto overflow-hidden bg-white">
           <Header />
           {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,35 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    fill: (theme) => ({
-      hover: theme('colors.gray.300'),
-      focus: theme('colors.gray.800'),
-    }),
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-    colors: {
-      white: '#FFFFFF',
-      hoverGray: '#F1F4F9',
-      mainPurple: '#8075F8',
-      lightGray: '#E9ECF0',
-      gray: '#C6C6C6',
-      darkGray: '#797979',
-      dark: '#333333',
+export const content = [
+  './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+];
+export const theme = {
+  fill: (theme) => ({
+    hover: theme('colors.gray.300'),
+    focus: theme('colors.gray.800'),
+  }),
+  fontFamily: {
+    sans: ['Noto Sans', 'sans-serif'],
+  },
+  extend: {
+    backgroundImage: {
+      'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      'gradient-conic':
+        'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
     },
   },
-  variants: { fill: ['hover', 'focus'] },
-  plugins: [],
+  colors: {
+    white: '#FFFFFF',
+    hoverGray: '#F1F4F9',
+    mainPurple: '#8075F8',
+    lightGray: '#E9ECF0',
+    gray: '#C6C6C6',
+    darkGray: '#797979',
+    dark: '#333333',
+  },
 };
+export const variants = { fill: ['hover', 'focus'] };
+export const plugins = [];
