a16ed831169215ab8bc9cca89f2f99696830feed
kimilgon <moses011010@gmail.com>
2023-07-07 00:00:08 +0900

    feat: EditModal을 Image->canvas로 변경

--- src/components/EditModal.tsx
+++ src/components/EditModal.tsx
@@ -1,22 +1,37 @@
 import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
 
-const EditModal = ({
-  onCloseModal,
-  canvasData,
-}: {
+type EditModalProps = {
   onCloseModal: () => void;
   canvasData: {
     canvasURL: string;
     canvasWidth: number;
     canvasHeight: number;
   };
-}) => {
+};
+
+const EditModal = ({ onCloseModal, canvasData }: EditModalProps) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext('2d');
+    const img = document.createElement('img');
+    img.src = canvasData.canvasURL;
+
+    if (canvas && ctx) {
+      canvas.width = canvasData.canvasWidth;
+      canvas.height = canvasData.canvasHeight;
+      ctx.drawImage(img, 0, 0, canvasData.canvasWidth, canvasData.canvasHeight);
+    }
+  }, []);
+
   return (
     <>
       <div className="fixed inset-0 z-50 bg-white rounded max-w-[540px] max-h-screen w-[90%] h-[90%] m-auto shadow">
         <div className="w-full h-full">
           <div
-            className={`box-border flex flex-row relative justify-center pt-11 max-h-screen  mx-auto`}
+            className={`box-border flex flex-row relative justify-center pt-11 max-h-screen mx-auto`}
           >
             <button
               className="absolute flex items-center justify-center w-8 h-8 border rounded-full top-1 right-2 right text-gray border-gray"
@@ -25,7 +40,15 @@ const EditModal = ({
               X
             </button>
             <div className="relative flex justify-center">
-              <Image
+              <canvas
+                ref={canvasRef}
+                style={{
+                  objectFit: 'contain',
+                  maxWidth: 'auto',
+                  maxHeight: '70vh',
+                }}
+              ></canvas>
+              {/* <Image
                 style={{
                   objectFit: 'contain',
                   maxWidth: 'auto',
@@ -35,7 +58,8 @@ const EditModal = ({
                 height={canvasData.canvasHeight}
                 src={canvasData.canvasURL}
                 alt="image"
-              />
+              /> */}
+              {/* </canvas> */}
             </div>
           </div>
         </div>
