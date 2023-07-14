dc67b5a89b0ea7300b3f11e80c005ff6c9ee03a1
kimilgon <moses011010@gmail.com>
2023-07-14 18:17:28 +0900

    change gatsby

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,13 +3,36 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `PocketPT-Gatsby`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `jgjgill-blog`,
+        short_name: `jgjgill-blog`,
+        start_url: `/`,
+        background_color: `#f0abfc`,
+        theme_color: `#c471f5`,
+        display: `standalone`,
+        icon: "src/images/icon.png",
+        cache_busting_mode: "none",
+        icon_options: {
+          // For all the options available,
+          // please see the section "Additional Resources" below.
+          purpose: `any maskable`,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+    },
+  ],
 };
 
 export default config;
