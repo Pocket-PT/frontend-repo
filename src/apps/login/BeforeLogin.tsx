c09ba9f6326da27e4e0192ff9281c3fb1bedc609
kimilgon <moses011010@gmail.com>
2023-10-12 14:19:44 +0900

    log : 회원가입 실패 시 알림

--- src/apps/login/BeforeLogin.tsx
+++ src/apps/login/BeforeLogin.tsx
@@ -322,6 +322,9 @@ const FinalStep = ({ inputName, phoneNumber }: FinalStepProps) => {
           alert('회원가입이 완료되었습니다.');
           replaceTo('Main');
         },
+        onError: () => {
+          alert('회원가입에 실패했습니다.');
+        },
       },
     );
   };
